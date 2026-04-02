dc30296d8a080f77d4bfff0db0ee168403c2bbb4
S1rEx1 <amenhotepyous@gmail.com>
2026-04-02 16:17:13 +0300

    simple tui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    event,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{
+    backend::CrosstermBackend,
+    layout::Alignment,
+    widgets::{Block, Borders, Paragraph},
+    Frame, Terminal,
+};
+use std::io::{self, stdout};
+
+fn main() -> io::Result<()> {
+    enable_raw_mode()?;
+    stdout().execute(EnterAlternateScreen)?;
+
+    let backend = CrosstermBackend::new(stdout());
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|frame: &mut Frame<'_>| {
+        let paragraph = Paragraph::new("Hello, Arch!")
+            .block(
+                Block::default()
+                    .title(" Installer ")
+                    .borders(Borders::ALL)
+            )
+            .alignment(Alignment::Center);
+
+        frame.render_widget(&paragraph, frame.size());
+    })?;
+
+    event::read()?;
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+
+    Ok(())
 }
