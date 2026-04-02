d6f124a1249ba606cd89674ae5a12ce332eedfef
S1rEx1 <amenhotepyous@gmail.com>
2026-04-02 17:03:02 +0300

    pwd

--- src/main.rs
+++ src/main.rs
@@ -19,14 +19,23 @@ fn main() -> io::Result<()> {
 
     let mut hostname = String::new();
     let mut username = String::new();
-    let mut running = true;
+    let mut password = String::new();
+    let mut password_conf = String::new();
 
-    let mut active_field: usize = 0; // 0 = username, 1 = hostname
+    let mut running = true;
+    let mut af = 0;
+    // af - active field
+    // 0 = username
+    // 1 = hostname
+    // 2 = password
+    // 3 = password confirmation
 
 
     while running {
         terminal.draw(|frame: &mut Frame<'_>| {
-            let display_text = format!("{}Username: {}\n {}Hostname: {}", (if active_field == 0 {">"} else {" "}),username, (if active_field == 1 {">"} else {" "}),hostname);
+            let display_text = format!("{}Username: {}\n {}Hostname: {}\n {}Password: {}\n {}Confirm your password: {}",
+                (if af == 0 {">"} else {" "}),username, (if af == 1 {">"} else {" "}),hostname,
+                (if af == 2 {">"} else {" "}),("*".repeat(password.len())), (if af == 3 {">"} else {" "}), password_conf);
 
             let paragraph = Paragraph::new(display_text)
                 .block(Block::default().title(" Setup ").borders(Borders::ALL))
@@ -38,29 +47,37 @@ fn main() -> io::Result<()> {
         if event::poll(std::time::Duration::from_millis(5))? {
             if let Event::Key(key) = event::read()? {
                 match key.code {
-                    KeyCode::Down => active_field = {
-                        if active_field == 0 {
-                            1
-                        } else { 0 }
+                    KeyCode::Down => {
+                        if af < 3 {
+                            af += 1;
+                        } else { af = 0 }
                     },
-                    KeyCode::Up => active_field = {
-                        if active_field == 0 {
-                            1
-                        } else { 0 }
-                    },
+                    KeyCode::Up => {
+                        if af > 0 {
+                            af -= 1;
+                           } else { af = 3 }
+                        },
                     KeyCode::Esc => running = false,
                     KeyCode::Char(c) => {
-                        if active_field == 0 {
+                        if af == 0 {
                             username.push(c);
-                        } else {
+                        } else if af == 1 {
                             hostname.push(c);
+                        } else if af == 2 {
+                            password.push(c);
+                        } else if af == 3 {
+                            password_conf.push(c);
                         }
                     },
                     KeyCode::Backspace => {
-                        if active_field == 0{
+                        if af == 0{
                             username.pop();
-                        } else {
+                        } else if af == 1 {
                             hostname.pop();
+                        } else if af == 2 {
+                            password.pop();
+                        } else if af == 3 {
+                            password_conf.pop();
                         }
                     }
                     _ => {}
@@ -72,6 +89,6 @@ fn main() -> io::Result<()> {
     stdout().execute(LeaveAlternateScreen)?;
     disable_raw_mode()?;
 
-    println!("\n username : {}\n hostname : {}", username, hostname);
+    println!("\n username : {}\n hostname : {}\n password : {}", username, hostname, password);
     Ok(())
 }
